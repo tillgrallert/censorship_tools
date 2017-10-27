677afdc55dfcffdbf391d313d4beb98a0d9652a5
Till Grallert <till.grallert@fu-berlin.de>
2017-10-27 18:27:08 +0300

    added layered plot of annual totals

--- r/plot_censorship.R
+++ r/plot_censorship.R
@@ -5,6 +5,8 @@ library(ggplot2)  # for creating graphs
 library(scales)   # to access breaks/formatting functions
 library(gridExtra) # for arranging plots
 library(plotly) # interactive plots based on ggplot
+library(dplyr) # for data cleaning
+library(plyr)
 
 # function to create subsets for periods
 funcPeriod <- function(f,x,y){f[f$date >= x & f$date <= y,]}
@@ -19,9 +21,6 @@ vCensorship <- read.csv("csv/censorship-levant.csv", header=TRUE, sep = ",", quo
 vCensorship$date <- as.Date(vCensorship$date)
 
 # aggregate periods
-## there is an option to add a column that contains the year as string, but this is not particularly helpful
-vCensorship$year <-format(vCensorship$date,format="%Y")
-
 ## use cut() to generate summary stats for time periods
 ## create variables of the year, quarter week and month of each observation:
 vCensorship$year <- as.Date(cut(vCensorship$date,
@@ -44,12 +43,48 @@ vDateStop <- as.Date("1914-12-31")
 vCensorshipPeriod <- funcPeriod(vCensorship,vDateStart,vDateStop)
 
 ## create a subset of rows based on conditions
-vCensorshipWarnings <- subset(vCensorshipPeriod,action=="W")
-vCensorshipSuspensions <- subset(vCensorshipPeriod,action=="S")
 vCensorshipPermits <- subset(vCensorshipPeriod,action=="P")
+vCensorshipSuspensions <- subset(vCensorshipPeriod,action=="S")
+vCensorshipWarnings <- subset(vCensorshipPeriod,action=="W")
+
+
+# calculate totals
+## annual total
+vCensorshipPermitsAnnual <- count(vCensorshipPermits,'year')
+vCensorshipSuspensionsAnnual <- count(vCensorshipSuspensions,'year')
+vCensorshipWarningsAnnual <- count(vCensorshipWarnings,'year')
 
 # plot
-## plot a time series
+## plot frequencies of actions
+plotActionFrequency <- ggplot(vCensorshipPeriod)+
+  labs(title="Censorship in Bilad al-Sham", 
+       subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Frequency")+ # provides title, subtitle, x, y, caption
+  # layer: suspensions
+  geom_line(data=vCensorshipSuspensionsAnnual,
+            aes(x=year, y=freq), # color, size, shape, stroke can be made dependent on columns
+            na.rm=TRUE,
+            color="red",
+            size=1)+
+  # layer: warnings
+  geom_line(data=vCensorshipWarningsAnnual,
+            aes(x=year, y=freq), # color, size, shape, stroke can be made dependent on columns
+            na.rm=TRUE,
+            color="purple",
+            size=1)+
+  # layer: permits
+  geom_line(data=vCensorshipPermitsAnnual,
+            aes(x=year, y=freq), # color, size, shape, stroke can be made dependent on columns
+            na.rm=TRUE,
+            color="green",
+            size=1)+
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"))+ #,
+               # limits=as.Date(c(vDateStart, vDateStop))) +
+  theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+plotActionFrequency
+## plot a time series for all 
 plotCensorshipTime <- ggplot(vCensorshipPeriod, aes(x = quarter, y = action)) +
   labs(title="Censorship in Bilad al-Sham", 
        # subtitle="based on announcements in newspapers", 
@@ -64,20 +99,30 @@ plotCensorshipTime <- ggplot(vCensorshipPeriod, aes(x = quarter, y = action)) +
   theme(legend.position="None")  # remove legend
 plotCensorshipTime
 
-## bar plot
-plotCensorshipBar <- ggplot(vCensorshipPeriod, aes(x = action, fill = action)) +
-  ggtitle("Censorship in Bilad al-Sham") +
-  geom_bar() +
+## bar plot: one bar for each action
+plotCensorshipBar <- ggplot(vCensorshipPeriod, aes(x = action)) +
+  labs(title="Censorship in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Action", 
+       y="Aggregate number of incidents")+ # provides title, subtitle, x, y, caption
+  geom_bar(aes(fill=action)) +
+  # geom_density(aes(fill=action))+ # density makes no sense in this context
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+plotCensorshipBar
   
 ## bar plot for aggregated periods using `stat_sum`
-plotCensorshipBarAggr <- ggplot(data = vCensorship, aes(x = year, # selects the period
-                                       y=action, fill=action)) +
-  ggtitle("Censorship in Bilad al-Sham") + # plot label
-  xlab("Date") + ylab("Action") + # axis labels
-  stat_summary(fun.y = sum, # adds up all observations for the period
-               geom = "bar", # or "line"
+plotCensorshipBarAggr <- ggplot(data = vCensorshipPeriod, aes(x = year, # selects the period
+                                       y=action,
+                                       color=action)) +
+  labs(title="Censorship in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Action")+ # provides title, subtitle, x, y, caption
+  stat_summary(data=subset(vCensorshipPeriod,action=="S"),
+               fun.y = sum, # adds up all observations for the period
+               geom = "line", # or "line"
                na.rm=TRUE) + # remove all empty/invalid levels
+  #coord_cartesian(ylim = c(0, 20)) +
   #geom_text(label=stat_summary(fun.y = sum),
   #          vjust = -1) + # adding values to the top of the bars
   #geom_label(aes(label=action), 
@@ -89,7 +134,7 @@ plotCensorshipBarAggr <- ggplot(data = vCensorship, aes(x = year, # selects the 
   theme_bw() # make the themeblack-and-white rather than grey
 plotCensorshipBarAggr
 
-## trials
+## Plot individual actions
 plotCensorshipTime1 <- ggplot(subset(vCensorshipPeriod,action=="P"), aes(x = date, y = action)) +
   ggtitle("Censorship in Bilad al-Sham") +
   xlab("Date") + ylab("Action") +
@@ -98,5 +143,3 @@ plotCensorshipTime1 <- ggplot(subset(vCensorshipPeriod,action=="P"), aes(x = dat
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
 plotCensorshipTime1
 
-# output
-plotCensorshipBar
