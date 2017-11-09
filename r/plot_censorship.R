fdb79094daeac3a58cd6d967e9d0e7d46225f90e
Till Grallert <till.grallert@fu-berlin.de>
2017-11-09 15:12:15 +0200

    added variables to quickly plot data by geographic region

--- r/plot_censorship.R
+++ r/plot_censorship.R
@@ -5,23 +5,25 @@ library(ggplot2)  # for creating graphs
 library(scales)   # to access breaks/formatting functions
 library(gridExtra) # for arranging plots
 library(plotly) # interactive plots based on ggplot
+library(dplyr) # for data cleaning
+#library(plyr)
 
 # function to create subsets for periods
 funcPeriod <- function(f,x,y){f[f$date >= x & f$date <= y,]}
 
 # use a working directory
-setwd("/Volumes/Dessau HD/BachUni/research-projects/censorship/censorship_data")
+#setwd("/Volumes/Dessau HD/BachUni/research-projects/censorship/censorship_data")
+setwd("/BachUni/research-projects/censorship/censorship_data")
 
 # 1. read price data from csv, note that the first row is a date
 vCensorship <- read.csv("csv/censorship-levant.csv", header=TRUE, sep = ",", quote = "")
+vLaws <- read.csv("csv/laws-press-ottoman-empire.csv", header=TRUE, sep = ",", quote = "")
 
 # convert date to Date class
 vCensorship$date <- as.Date(vCensorship$date)
+vLaws$date <- as.Date(vLaws$date)
 
 # aggregate periods
-## there is an option to add a column that contains the year as string, but this is not particularly helpful
-vCensorship$year <-format(vCensorship$date,format="%Y")
-
 ## use cut() to generate summary stats for time periods
 ## create variables of the year, quarter week and month of each observation:
 vCensorship$year <- as.Date(cut(vCensorship$date,
@@ -34,22 +36,139 @@ vCensorship$week <- as.Date(cut(vCensorship$date,
                                 breaks = "week",
                                 start.on.monday = TRUE)) # allows to change weekly break point to Sunday
 
+## do the same for laws
+vLaws$year <- as.Date(cut(vLaws$date,
+                                breaks = "year"))
+vLaws$quarter <- as.Date(cut(vLaws$date,
+                                   breaks = "quarter"))
+vLaws$month <- as.Date(cut(vLaws$date,
+                                 breaks = "month"))
+
 # select rows
 ## select the first row (containing dates), and the rows containing prices in ops
 # v_wheatKileSimple <- v_wheatKile[,c("date","quantity.2","quantity.3")]
 
 ## specify period
-vDateStart <- as.Date("1875-01-01")
-vDateStop <- as.Date("1914-12-31")
+vDateStart <- as.Date("1857-01-01")
+vDateStop <- as.Date("1916-12-31")
 vCensorshipPeriod <- funcPeriod(vCensorship,vDateStart,vDateStop)
+vLawsPeriod <- funcPeriod(vLaws,vDateStart,vDateStop)
+
 
 ## create a subset of rows based on conditions
-vCensorshipWarnings <- subset(vCensorshipPeriod,action=="W")
-vCensorshipSuspensions <- subset(vCensorshipPeriod,action=="S")
 vCensorshipPermits <- subset(vCensorshipPeriod,action=="P")
+vCensorshipSuspensions <- subset(vCensorshipPeriod,action=="S")
+vCensorshipWarnings <- subset(vCensorshipPeriod,action=="W")
+
+
+# calculate totals
+## annual total
+vCensorshipPermitsAnnual <- count(vCensorshipPermits,'year')
+vCensorshipSuspensionsAnnual <- count(vCensorshipSuspensions,'year')
+vCensorshipWarningsAnnual <- count(vCensorshipWarnings,'year')
+
+# limit to a specific location
+## Bil??d al-Sh??m
+vLevant <- subset(vCensorshipPeriod, location %in% c('Aleppo','Ba??bd??','Beirut','Damascus','Haifa','Jaffa','Jerusalem','Tripoli', 'Ottoman Empire'))
+## Egypt
+vEgypt <- subset(vCensorshipPeriod, location %in% c('Alexandria', 'Cairo', 'Egypt', 'Port Said'))
+vMaghrib <- subset(vCensorshipPeriod, location %in% c('ALgiers', 'Tunis'))
+vAbroad <- subset(vCensorshipPeriod, location %in% c('Buenos Aires','Livorno','London','Naples','New York','Paris', 'Rio di Janeiro'))
+vEmpire <- subset(vCensorshipPeriod, location %in% c('Constantinople','Istanbul','Ottoman Empire'))
+# Yemen, Iraq, Iran, India still missing
+
+# grouping types of action: Levant
+vLevantRestrictive <- subset(vLevant, action %in% c('S','W','Trial','Raid','BI','CP'))
+vLevantPermissive <- subset(vLevant, action %in% c('P','PI','PR','RP'))
+vLevantPermits <- subset(vLevant,action %in% c('P'))
+vLevantSuspensions <- subset(vLevant,action %in% c('S'))
+vLevantWarnings <- subset(vLevant,action %in% c('W'))
+
+# grouping types of action: Egypt
+vEgyptRestrictive <- subset(vEgypt, action %in% c('S','W','Trial','Raid','BI','CP'))
+vEgyptPermissive <- subset(vEgypt, action %in% c('P','PI','PR','RP'))
+vEgyptPermits <- subset(vEgypt,action %in% c('P'))
+vEgyptSuspensions <- subset(vEgypt,action %in% c('S'))
+vEgyptWarnings <- subset(vEgypt,action %in% c('W'))
 
 # plot
-## plot a time series
+## plot frequencies of actions
+### histogram: restrictions in the Levant
+plotLevantRestrictive <- ggplot()+
+  labs(title="Censorship in Bilad al-Sham", 
+       subtitle="Restrictions", 
+       x="Date", 
+       y="Frequency")+ # provides title, subtitle, x, y, caption
+  geom_bar(data=vLevantRestrictive, aes(x=year,fill=action),position = "stack",width = 200)+
+  #geom_bar(data=vCensorshipSuspensions, aes(x=year,fill=action),position = "stack",width = 200)+
+  #geom_bar(data=vCensorshipWarnings, aes(x=year,fill=action),position = "stack",width = 200)+
+  # layer: legal framework
+  geom_jitter(data=vLawsPeriod,(aes(x=year,y=10)),size=3)+
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"))+ #,
+  # limits=as.Date(c(vDateStart, vDateStop))) +
+  scale_y_continuous(breaks = waiver())+
+  theme_bw()
+plotLevantRestrictive
+
+### histogramm: permissions in the Levant
+plotLevantPermissive <- ggplot()+
+  labs(title="Censorship in Bilad al-Sham", 
+       subtitle="Permissions", 
+       x="Date", 
+       y="Frequency")+ # provides title, subtitle, x, y, caption
+  geom_bar(data=vLevantPermissive, aes(x=year, fill=action),position = "stack", width = 200)+
+  #geom_bar(data=vCensorshipPermits, aes(x=year, fill=action),position = "stack", width = 200)+
+  # layer: legal framework
+  geom_jitter(data=vLawsPeriod,(aes(x=year,y=10)),size=3)+
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"))+ #,
+  # limits=as.Date(c(vDateStart, vDateStop))) +
+  scale_y_continuous(breaks = waiver())+
+  theme_bw()
+plotLevantPermissive
+
+### histogram: restrictions in Egypt
+plotEgyptRestrictive <- ggplot()+
+  labs(title="Censorship in Bilad al-Sham", 
+       subtitle="Restrictions", 
+       x="Date", 
+       y="Frequency")+ # provides title, subtitle, x, y, caption
+  geom_bar(data=vEgyptRestrictive, aes(x=year,fill=action),position = "stack",width = 200)+
+  #geom_bar(data=vCensorshipSuspensions, aes(x=year,fill=action),position = "stack",width = 200)+
+  #geom_bar(data=vCensorshipWarnings, aes(x=year,fill=action),position = "stack",width = 200)+
+  # layer: legal framework
+  geom_point(data=vLawsPeriod,(aes(x=date,y=10)),size=3)+
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"))+ #,
+  # limits=as.Date(c(vDateStart, vDateStop))) +
+  scale_y_continuous(breaks = waiver())+
+  theme_bw()
+plotEgyptRestrictive
+
+### use individual layers
+plotActionFrequency <- ggplot()+
+  labs(title="Censorship in Bilad al-Sham", 
+       subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Frequency")+ # provides title, subtitle, x, y, caption
+  # layer: suspensions
+  geom_line(data=count(subset(vLevant,action=="S"),'year'),aes(x=year, y=freq),na.rm=TRUE,color="#E32006",size=1)+
+  #geom_bar(data=count(subset(vLevant,action=="S"),'year'), stat='identity', aes(x=year, y=freq), na.rm=TRUE, width = 5, color="#E32006", fill="#E32006")+
+  # layer: warnings
+  geom_line(data=count(subset(vLevant,action=="W"),'year'),aes(x=year, y=freq),na.rm=TRUE,color="#871020",size=1)+
+  #geom_bar(data=count(subset(vLevant,action=="W"),'year'), stat='identity', aes(x=year, y=freq), na.rm=TRUE, width = 5, color="#871020", fill="#871020")+
+  # layer: permits
+  geom_line(data=count(subset(vLevant,action=="P"),'year'),aes(x=year, y=freq),na.rm=TRUE,color="#209A00",size=1)+
+  #geom_bar(data=count(subset(vLevant,action=="P"),'year'), stat='identity', aes(x=year, y=freq), na.rm=TRUE, width = 5, color="#209A00", fill="#209A00")+
+  scale_x_date(breaks=date_breaks("2 years"), 
+               labels=date_format("%Y"))+ #,
+               # limits=as.Date(c(vDateStart, vDateStop))) +
+  scale_y_continuous(breaks = waiver())+
+  theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
+plotActionFrequency
+
+## plot a time series for all 
 plotCensorshipTime <- ggplot(vCensorshipPeriod, aes(x = quarter, y = action)) +
   labs(title="Censorship in Bilad al-Sham", 
        # subtitle="based on announcements in newspapers", 
@@ -64,22 +183,30 @@ plotCensorshipTime <- ggplot(vCensorshipPeriod, aes(x = quarter, y = action)) +
   theme(legend.position="None")  # remove legend
 plotCensorshipTime
 
-## bar plot
+## bar plot: one bar for each action
 plotCensorshipBar <- ggplot(vCensorshipPeriod, aes(x = action)) +
-  ggtitle("Censorship in Bilad al-Sham") +
+  labs(title="Censorship in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Action", 
+       y="Aggregate number of incidents")+ # provides title, subtitle, x, y, caption
   geom_bar(aes(fill=action)) +
   # geom_density(aes(fill=action))+ # density makes no sense in this context
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
 plotCensorshipBar
   
 ## bar plot for aggregated periods using `stat_sum`
-plotCensorshipBarAggr <- ggplot(data = vCensorship, aes(x = year, # selects the period
-                                       y=action, fill=action)) +
-  ggtitle("Censorship in Bilad al-Sham") + # plot label
-  xlab("Date") + ylab("Action") + # axis labels
-  stat_summary(fun.y = sum, # adds up all observations for the period
-               geom = "bar", # or "line"
+plotCensorshipBarAggr <- ggplot(data = vCensorshipPeriod, aes(x = year, # selects the period
+                                       y=action,
+                                       color=action)) +
+  labs(title="Censorship in Bilad al-Sham", 
+       # subtitle="based on announcements in newspapers", 
+       x="Date", 
+       y="Action")+ # provides title, subtitle, x, y, caption
+  stat_summary(data=subset(vCensorshipPeriod,action=="S"),
+               fun.y = sum, # adds up all observations for the period
+               geom = "line", # or "line"
                na.rm=TRUE) + # remove all empty/invalid levels
+  #coord_cartesian(ylim = c(0, 20)) +
   #geom_text(label=stat_summary(fun.y = sum),
   #          vjust = -1) + # adding values to the top of the bars
   #geom_label(aes(label=action), 
@@ -91,7 +218,7 @@ plotCensorshipBarAggr <- ggplot(data = vCensorship, aes(x = year, # selects the 
   theme_bw() # make the themeblack-and-white rather than grey
 plotCensorshipBarAggr
 
-## trials
+## Plot individual actions
 plotCensorshipTime1 <- ggplot(subset(vCensorshipPeriod,action=="P"), aes(x = date, y = action)) +
   ggtitle("Censorship in Bilad al-Sham") +
   xlab("Date") + ylab("Action") +
@@ -100,5 +227,3 @@ plotCensorshipTime1 <- ggplot(subset(vCensorshipPeriod,action=="P"), aes(x = dat
   theme_bw() # make the themeblack-and-white rather than grey (do this before font changes, or it overridesthem)
 plotCensorshipTime1
 
-# output
-plotCensorshipBar
